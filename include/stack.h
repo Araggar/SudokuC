b7a3340ac66ba9ecde866e2e1b02c15a44adf5d8
Araggar <lucas.mayr.athayde@gmail.com>
2018-05-26 21:21:14 -0300

    Add Includes

---
+++ include/stack.h
@@ -0,0 +1,32 @@
+#ifndef STACK_H
+#define STACK_H 
+#include <pthread.h>
+#include <stdlib.h>
+
+typedef struct StructData {
+	unsigned int* state;
+	unsigned int start_ind;
+} Data;
+
+typedef struct node_t
+{
+    Data * data;
+    struct node_t * next;
+} Node;
+
+typedef struct stack_t
+{
+	Node * top;
+	unsigned int size;
+	unsigned int multiplier;
+	unsigned int threshold;
+	pthread_mutex_t mutex;
+
+} Stack;
+
+void stackinit(Stack*);
+void stackpush(Stack*, Data*);
+Data * stackpop(Stack*);
+void stackdispose(Stack*);
+
+#endif
